b40af7f8b5b10b0d8a766b4785817ce781d96c04
pjatm <113640129+pjatm@users.noreply.github.com>
2025-04-24 16:49:57 -0500

    Add files via upload

---
+++ Magic 8 ball.py
@@ -0,0 +1,24 @@
+# Write code below 💖
+import random
+num = random.randint(1,9)
+ask = input("Question: ")
+if num == 1:
+  print("Magic 8 Ball: Yes - definitely")
+if num == 2:
+  print("Magic 8 Ball: It is decidedly so")
+if num == 3:
+  print("Magic 8 Ball: Without a doubt")
+if num == 4:
+  print("Magic 8 Ball: Outlook not so good")
+if num == 5:
+  print("Magic 8 Ball: Ask again later")
+if num == 6:
+  print("Magic 8 Ball: Reply hazy, try again")
+if num == 7:
+  print("Magic 8 Ball: My sources say no")
+if num == 8:
+  print("Magic 8 Ball: Better not tell you now")
+if num == 9:
+  print("Magic 8 Ball: Very doubtful")
+
+print("Magic 8 Ball: ", num)
